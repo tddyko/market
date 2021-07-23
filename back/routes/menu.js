4d6f5d94e1caf6d76181790f8e60e2bf6eff2efd
lilillii <kibum0726@naver.com>
2021-07-23 20:19:59 +0900

    수정 중

---
+++ back/routes/menu.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const {v4: uuidv4} = require('uuid');
+const {Prodouct,Store_infrom} = require('../models');
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
+
+//localhost:3000/menu/addmenu
+//id 가 name , price인 곳에서 정보를 받음
+router.get('/addMenu', isLoggedIn, async(req,res)=>{
+    Prodouct.create({
+        product_id : uuidv4(),
+        market_id : req.user.market_id,
+        name : req.body.name,
+        price : req.body.price,
+    }).then(r => {
+        if(r)
+            console.log('메뉴 추가 성공');
+        else
+            console.log('메뉴 추가 실패');
+    }).catch(err=>console.dir(err));
+});
+
+//localhost:3000/menu/update
+//id 가 name , price인 곳에서 정보를 받음
+router.get('/update', isLoggedIn, async(req,res)=>{
+    findId = req
+    Prodouct.update({
+        name : req.body.nalme,
+        price : req.body.price
+    },
+    {
+        where : {
+            store_id : req.user.store_id,   
+        }
+    }).then(r => {
+        if(r)
+        console.log('수정 성공');
+    }).catch(err => console.dir(err));
+
+})
+module.exports = router;
