e24dd98e270e65694ecd37c56649b3bc6b9301a1
DESKTOP-1972H69\Administrator <corian1516@gmail.com>
2021-07-28 03:20:40 +0900

    뷰 노드 연결 다시 세팅함

---
+++ front/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000/api',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
+
+    outputDir: '../back/public',
+
+    transpileDependencies: [
+      'vuetify'
+    ]
+}
