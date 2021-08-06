d0e15e8edfb57b249e78ff395f3425ea6ce430c8
이정민 <als8943@gmail.com>
2021-08-06 13:32:56 +0900

    로그인만드는중

---
+++ front/src/layouts/reservate/view.vue
@@ -0,0 +1,17 @@
+<template>
+<v-app>
+  <rservate-view />
+
+  </v-app>
+</template>
+
+<script>
+
+export default {
+ name: 'reservateLayout'
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/views/market/로그인.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container
+    
+    fill-height
+  >
+    <v-row
+      justify="center"
+    >
+      <v-col
+        cols=""
+        xs="10"
+        sm="6"
+        md="6"
+        lg="6"
+        xl="6"
+      >
+        <v-card
+          class="pa-5 "
+
+          elevation="5"
+          fill-height
+          width="500"
+          height="650"
+        >
+          <v-card-text>
+            <h1>
+              로그인
+            </h1>
+          </v-card-text>
+          <v-text-field          
+            label="이메일"
+          />
+          <v-text-field
+            label="비밀번호"
+          />
+          <v-btn
+            large
+            block
+            color="primary"
+            rounded
+          >
+            로그인
+          </v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
