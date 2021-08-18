a6488480d483456a33877000c5299a3e9385d0d1
고민혁 <corian1516@gmail.com>
2021-08-18 16:43:20 +0900

    불필요 파일 삭제

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -29,59 +29,11 @@ const routes = [
       import(/* webpackChunkName: "Order" */ "../views/market/OrderReviewManagement.vue"),
   },
   {
-    path: "/market/basic",
-    name: "basic",
-    component: () =>
-      import(/* webpackChunkName: "Reservation" */ "@/views/market/기본정보.vue"),
-  },
-  {
-    path: "/market/basic2",
-    name: "basic",
-    component: () =>
-      import(/* webpackChunkName: "Reservation" */ "@/views/market/기본정보2.vue"),
-  },
-  {
-    path: "/market/order2",
-    name: "order2",
-    component: () =>
-      import(/* webpackChunkName: "order2" */ "@/views/market/주문내역.vue"),
-  },
-  {
-    path: "/market/login",
-    name: "login",
-    component: () =>
-      import(/* webpackChunkName: "order2" */ "@/views/market/로그인.vue"),
-  },
-  {
-    path:'/market/reservate',
-      component: () => import(
-        /* webpackChunkName: "layouts-reservate0" */
-        '@/layouts/reservate/view'
-        ),
-    children: [
-        {
-            path:'reservate1',
-            name:'reservate1',
-            component: () => import(/* webpackChunkName: "views-reservate1" */
-            '@/views/market/reservate/리뷰관리')
-
-        },
-        {
-            path:'reservate2',
-            name:'reservate2',
-            component: () => import(/* webpackChunkName: "views-reservate2" */
-            '@/views/market/reservate/예약내역')
-
-        },
-
-    ]
-},
-{
-  path:'/market/defaultinfo',
-  name:'dafaultinfo',
-  component: () => import(/* webpackChunkName: "views-reservate2" */
-  '@/views/market/DefaultInfo.vue')
-}
+    path: '/market/defaultinfo',
+    name: 'dafaultinfo',
+    component: () => import(/* webpackChunkName: "views-reservate2" */
+      '@/views/market/DefaultInfo.vue')
+  }
 ];
 
 const router = new VueRouter({
