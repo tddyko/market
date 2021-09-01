de3f6fc0e47890cfd747dfbf5478497b5a4b8abb
고민혁 <corian1516@gmail.com>
2021-09-02 03:10:19 +0900

    디자인작업 완료

--- front/src/views/main/marketdetail/popup/order/OrderCart.vue
+++ front/src/views/main/marketdetail/popup/order/OrderCart.vue
@@ -32,10 +32,9 @@
       </v-col>
       <v-col cols="10">
         <v-text-field
-          hint="소스 많이 부어주세요, 6시 전까지 배달해주세요"
+          hide-details
           label="주문 상세 요청 기입"
           outlined
-          persistent-hint
         />
       </v-col>
     </v-row>
@@ -60,61 +59,72 @@
             justify="center"
           >
             <v-col
-              align="center"
-              cols="3"
+              class="pa-0 ma-0"
+              cols="2"
             >
               <v-img
-                class="rounded-lg"
-                height="110"
+                class="rounded-lg ma-2"
+                :height="`${Img_size}`"
                 lazy-src="https://picsum.photos"
                 src="https://picsum.photos/150"
-                width="110"
+                :width="`${Img_size}`"
               />
             </v-col>
             <v-col
               cols="5"
             >
-              <v-card-title>허니콤보</v-card-title>
-              <v-card-text class="text-left text--secondary">
+              <v-card-title :class="`text-${menuFont1} pa-0 my-3`">
+                허니콤보
+              </v-card-title>
+              <v-card-text :class="`text-${menuFont2} text-left text--secondary pa-0 my-3`">
                 메뉴 설명
               </v-card-text>
-              <v-card-text class="text-left font-weight-bold price">
+              <v-card-text :class="`text-${menuFont1} text-left font-weight-bold price pa-0 my-3`">
                 9,900원
               </v-card-text>
             </v-col>
             <v-col
-              cols="4"
+              cols="5"
+              align="end"
             >
               <v-btn
                 v-if="reservations_Number === 0"
                 :min-height="`${btn_Height_Size}`"
                 :min-width="`${btn_Width_Size}`"
-                class="btn-people pa-0"
+                class="btn-people pa-0 mr-4 ml-0"
                 disabled
                 outlined
                 @click="setReservations_number_minus"
               >
-                -
+                <label
+                  :class="`text-${countSize}`"
+                >-</label>
               </v-btn>
               <v-btn
                 v-else
                 :min-height="`${btn_Height_Size}`"
                 :min-width="`${btn_Width_Size}`"
-                class="btn-people pa-0"
+                class="btn-people pa-0 mr-4 ml-0"
                 outlined
                 @click="setReservations_number_minus"
               >
-                -
+                <label
+                  :class="`text-${countSize}`"
+                >-</label>
               </v-btn>
-              <span>{{ reservations_Number }}</span>
+              <label
+                :class="`text-${countSize}`"
+              >{{ reservations_Number }}</label>
               <v-btn
                 :min-height="`${btn_Height_Size}`"
                 :min-width="`${btn_Width_Size}`"
-                class="btn-people pa-0"
+                class="btn-people pa-0 ml-4 mr-0"
                 outlined
                 @click="setReservations_number_plus"
               >
-                +
+                <label
+                  :class="`text-${countSize}`"
+                >+</label>
               </v-btn>
             </v-col>
           </v-row>
@@ -163,15 +173,15 @@ export default {
     btn_Width_Size() {
       switch (this.$vuetify.breakpoint.name) {
         case 'sm':
-          return '24'
+          return '44'
         case 'md':
-          return '64'
+          return '44'
         case 'lg':
-          return '64'
+          return '44'
         case 'xl':
-          return '64'
+          return '50'
         default :
-          return '20'
+          return '23'
         }
       },
     btn_Height_Size() {
@@ -183,11 +193,56 @@ export default {
         case 'lg':
           return '36'
         case 'xl':
-          return '36  '
+          return '36'
         default :
-          return '5'
+          return '16'
       }
-    }
+    },
+    countSize() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'sm':
+          return 'sm-body-2'
+        default :
+          return 'caption'
+      }
+    },
+    menuFont1() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'sm':
+          return 'sm-body-1'
+        case 'md':
+          return 'md-h6'
+        case 'lg':
+          return 'lh-h6'
+        case 'xl':
+          return 'xl-h6'
+        default :
+          return 'body-1'
+      }
+    },
+    menuFont2() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'sm':
+          return 'sm-caption'
+        case 'md':
+          return 'md-body-1'
+        case 'lg':
+          return 'lg-body-1'
+        case 'xl':
+          return 'xl-body-1'
+        default :
+          return 'caption'
+      }
+    },
+    Img_size(){
+      switch (this.$vuetify.breakpoint.name){
+        case 'sm' : return '90'
+        case 'md' : return '100'
+        case 'lg' : return '110'
+        case 'xl' : return '120'
+        default : return '60'
+      }
+    },
   },
   methods: {
     setReservations_number_plus(){
